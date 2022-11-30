7a5fbf273a6145cf2c873a8b856473bfff326df6
Wylken S. Machado <wylken.ufal@gmail.com>
2022-11-30 19:46:59 -0300

    Config global styles end eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-
+import { GlobalStyle } from './styles/global'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
-    <h1>Hello Wylken</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello Wylken</h1>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
