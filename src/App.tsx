c517e21a34e3159cab6635e27f0a3201febc2c3b
wylken <wylken.ufal@gmail.com>
2022-12-01 19:23:44 -0300

    add routes and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { GlobalStyle } from './styles/global'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello Wylken</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
